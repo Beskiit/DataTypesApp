59a2f70bf4d734e31940eb22d49eda472657f204
gamil.293092 <gamil.293092@LAB-B-PC11>
2024-02-16 08:49:46 +0800

    Add project files.

---
+++ DataTypesApp/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DataTypesApp
+{
+    class DataTypesProgram
+    {
+        static void Main(String[] args)
+        {
+            Console.Write("Enter the pieces of apple: ");
+            int pieces = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the total price of " + pieces + " apple(s): ");
+            double price = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("The total price of " + pieces + " apple(s) is " + price);
+            Console.WriteLine("The value of the original price is " + price);
+            Console.WriteLine("The value of the original price is " + (int) price);
+            Console.WriteLine("Press any key to exit......");
+            Console.ReadKey();
+        }
+    }
+}
